82882fcea93b63b7d4dfb58eec50dbcdcd5b53fd
dakbobo <dakbobo@gmail.com>
2013-11-13 02:56:31 +0800

    ADD html

---
+++ fibonacci.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="zh-TW">
+	<head>
+		<meta charset="utf-8">
+		<title>php</title>
+	</head>
+	<body>
+		<form method="get" action="index.php">
+		<label for="number1">數字</label>
+		<input type="text" id="number1" name="number1">
+		<br>
+		<input type="submit" value="確定">
+		<br/>
+		<?php if (isset($_GET['number1']) && isset($_GET['number2']) ):?>
+			<?php if (!is_numeric($_GET['number1'])): ?><p>請輸入數字</p><?php endif; ?>
+		</form>
+	</body>
+</html>
