f44928454cdd9b18eb2b228e7625d42ff1d484ac
dakbobo <dakbobo@gmail.com>
2013-11-13 03:51:40 +0800

    ADD function fibonacci

--- fibonacci.php
+++ fibonacci.php
@@ -5,15 +5,32 @@
 		<title>php</title>
 	</head>
 	<body>
-		<form method="get" action="index.php">
+		<form method="get" action="fibonacci.php">
 		<label for="number1">數字</label>
 		<input type="text" id="number1" name="number1">
 		<br>
 		<input type="submit" value="確定">
 		<br/>
-		<?php if (isset($_GET['number1']) && isset($_GET['number2']) ):?>
+		<?php if (isset($_GET['number1']) ):?>
 			<?php if (!is_numeric($_GET['number1'])): ?><p>請輸入數字</p><?php endif; ?>
-			<?php endif;?>
+		<?php 
+		 function fibonacci($number1){
+			$fibs=array(0,1);
+			for($i=2;$i<$n;$i++){
+				$fibs[$i]=$fibs[$i-1]+$fibs[$i-2];
+			}
+			foreach($fibs as $fib){
+				echo $fib.",";
+			}
+		}
+		fibonacci($number1)
+		?>
+			
+			
+			
+			
+		
+		<?php endif;?>
 		</form>
 	</body>
 </html>
