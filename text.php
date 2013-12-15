14034659ac08acd71ee40ed0995d8f2dc8f38602
dakbobo <dakbobo@gmail.com>
2013-12-16 01:17:41 +0800

    test git test.php

---
+++ text.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<form method="GET" action="text.php">
+數字: <input type="text" name="user">
+數字2: <input type="text" name="user1">
+<br/>
+<input type="submit" value="確定">
+<br>
+<?php echo "{$_GET['user']} * {$_GET['user1']} =";echo  $_GET['user'] * $_GET['user1']; ?>
+
+</form>
