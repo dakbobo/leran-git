a18865de192667020439831976c94f7aa1b2d820
dakbobo <dakbobo@gmail.com>
2013-12-16 18:59:40 +0800

    submit value 修改成 確定

--- fibonacci.php
+++ fibonacci.php
@@ -9,7 +9,7 @@
 		<label for="number1">number</label>
 		<input type="text" id="number1" name="number1">
 		<br>
-		<input type="submit" value="submit">
+		<input type="submit" value="確定">
 		<br/>
 		<?php if (isset($_GET['number1']) ):?>
 			<?php if (!is_numeric($_GET['number1'])): ?><p>請輸入數字</p><?php endif; ?>
