21c9af422a5e8d2e5da2a9a4ec00850d4bcda6cb
dakbobo <dakbobo@gmail.com>
2013-12-16 18:22:20 +0800

    submit value changed yes

--- text.php
+++ text.php
@@ -3,7 +3,7 @@
 數字: <input type="text" name="user">
 數字2: <input type="text" name="user1">
 <br/>
-<input type="submit" value="確定">
+<input type="submit" value="yes">
 <br>
 <?php echo "{$_GET['user']} * {$_GET['user1']} =";echo  $_GET['user'] * $_GET['user1']; ?>
 
